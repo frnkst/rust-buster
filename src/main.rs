52be0f374f71c04f22ade4ea9eab683554106c95
Frank <frank@void0.ch>
2022-06-15 21:17:16 +0200

    Small fix

--- src/main.rs
+++ src/main.rs
@@ -16,47 +16,38 @@ struct FileResult {
     extension: String
 }
 
-fn get_line_count(file_path: &str) -> usize {
-    let input = File::open(file_path).expect("Cannot open wordlist");
-    let buffered = BufReader::new(input);
-    let line_count = buffered.lines().count();
-    line_count
-}
-
 fn main() {
     let args = args::Args::parse();
+    let threads = usize::from(args.threads);
+    let extension_count = &args.extension.len();
+    let line_count = get_line_count(&args.wordlist);
 
     println!("{}{}", color::Fg(color::LightMagenta), banner::banner());
 
-    let threads = usize::from(args.threads);
-
-    let line_count = get_line_count(&args.wordlist);
-    let extension_count = &args.extension.len();
-
     let reader = BufReader::new(File::open(&args.wordlist).expect("Cannot open wordlist"));
-
     let pool = ThreadPool::new(threads);
     let (tx, rx) = channel();
 
     let now = Instant::now();
     for line in reader.lines() {
-        let l = line.unwrap();
-        for extension in &args.extension {
-            let tx = tx.clone();
-            let url = args.url.to_owned().clone();
-            let ext = extension.to_owned().clone();
-            let m = l.clone();
-            pool.execute(move|| {
-                let url = format!("{}{}{}.{}", url, "/", m, ext);
-                let status = reqwest::blocking::get(&url).unwrap().status();
-                let res = FileResult {
-                    status,
-                    path: m.to_string(),
-                    extension: ext.to_string()
-                };
+        if let Ok(l) = line {
+            for extension in &args.extension {
+                let tx = tx.clone();
+                let url = args.url.to_owned().clone();
+                let ext = extension.to_owned().clone();
+                let m = l.clone();
+                pool.execute(move || {
+                    let url = format!("{}{}{}.{}", url, "/", m, ext);
+                    let status = reqwest::blocking::get(&url).unwrap().status();
+                    let res = FileResult {
+                        status,
+                        path: m.to_string(),
+                        extension: ext.to_string()
+                    };
 
-                tx.send(res).expect("channel will be there waiting for the pool");
-            });
+                    tx.send(res).expect("channel will be there waiting for the pool");
+                });
+            }
         }
     }
 
@@ -65,4 +56,11 @@ fn main() {
     }
 
     println!("Done. [{} milliseconds]", now.elapsed().as_millis());
+}
+
+fn get_line_count(file_path: &str) -> usize {
+    let input = File::open(file_path).expect("Cannot open wordlist");
+    let buffered = BufReader::new(input);
+    let line_count = buffered.lines().count();
+    line_count
 }
